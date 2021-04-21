4f9943c402a9ebd916be5995a8ac076a018e5add
Nikolaev11 <72603956+Nikolaev11@users.noreply.github.com>
2021-04-21 14:30:49 +0700

    Update plain.js

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -3,7 +3,7 @@ import _ from 'lodash';
 const stringify = (value) => {
   if (_.isObject(value)) return '[complex value]';
   if (_.isBoolean(value)) return value;
-  // if (_.isNumber(value)) return value;
+  if (_.isNumber(value)) return value;
   if (_.isNull(value)) return 'null';
   return `'${value}'`;
 };
